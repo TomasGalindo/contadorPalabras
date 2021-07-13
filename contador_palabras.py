6fd2a0937f377c61a4aad9e457e71d591425c5de
TomasGalindo <tgalindoencinas@yahoo.com>
2021-07-13 20:33:40 +0200

    Terminado

--- contador_palabras.py
+++ contador_palabras.py
@@ -1,12 +1,26 @@
 """
 contador de palabras de un fichero o de una frase.
 Al final debe sacar la palabra que más veces aparezca.
+Se modifican todas las palabras para que estén en minúsculas, por lo tanto False y false seran la misma palabra
 """
+import sys
 import re
-
+# Si salta alguna excepcion devolvera None.
+def ReadFile(file):
+    id = 0
+    try:
+        with open(file,'r') as myFile:
+            file_content = myFile.read()
+            return file_content
+    except FileNotFoundError:
+        print("File Not Found")
+    except:
+        print("Unexpected error:" + str(sys.exc_info()))
+    
 #guarda en una lista todas las palabras separando por diferentes signos de puntuación
 def mySplit(text):
     b = re.split('[\[\].;,\-_()!?\s\n]',text) #Todos los simbolos de puntuacion
+    b[:] = (w.lower() for w in b) #convierte a minusculas cada palabra
     filtered_words = filter(None,b)
     return filtered_words
 
@@ -16,33 +30,25 @@ def addWord(word):
     else:
         my_words[word] =1
 
-def processLine(text):
-    #
+def processText(text):
     words = mySplit(text) 
-    #Recorro la lista de palabras
-    for p in b:
-        print(p)
+    for p in words:
         addWord(p)
 
 #Encuentra las palabras con la mayor frecuencia    
 def maxWords(dict):
     maxDetect = max(my_words.values())
     for word, freq in dict.items():
-        #print(word + " -- " + str(freq))
         if (freq == maxDetect):
             print(word + " -- " + str(freq))
 
-
-
 #Main
-my_words={} #diccionario donde voy a guardar mis palabras
-a= "hola qu]e tal    tabulo False.     hago de zz todo. 1,2,3,4,5, Hay que (quitar \n todos los simbolos) de puntuacion; para que solo queden las palabras"
-b= mySplit(a)
-
-#Recorro la lista de palabras
-for p in b:
-    #print(p)
-    addWord(p)
-
-
-maxWords(my_words)
+# leo el archivo
+my_text = ReadFile("archivo.txt")
+#my_text = "hóla hola Fálse Falsé, falsè )) fàlse flse "
+if(my_text):
+    my_words={} 
+    processText(my_text)
+    maxWords(my_words)
+else:
+    print ("NO elements")
